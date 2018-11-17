467c0d8d8460b673af2f0b0bf2929a8f71d59f6c
Joerez <rezenbiz@gmail.com>
2018-11-17 15:14:49 -0800

    added myself to the team

--- client/src/components/team/team.js
+++ client/src/components/team/team.js
@@ -63,6 +63,19 @@ const TeamCard = () => {
             <a href={'https://github.com/PhyllisWong'}>github</a>
           </CardBody>
         </Card>
+
+
+        <Card>
+          <CardImg top width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=256%C3%97180&w=256&h=180" alt="Card image cap" />
+          <CardBody>
+            <CardTitle>Joe Rezendes</CardTitle>
+            <CardSubtitle>Front End Developer</CardSubtitle>
+            <CardText>This card has supporting text below as a natural lead-in to additional content.</CardText>
+            <a href={'https://github.com/joerez'}>github</a>
+          </CardBody>
+        </Card>
+
+
         <Card>
           <CardImg top width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=256%C3%97180&w=256&h=180" alt="Card image cap" />
           <CardBody>
