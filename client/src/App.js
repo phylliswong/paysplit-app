65a2e816ec713603734bb21bbfeba2c9063b5ad5
Phyllis Wong <wong.phyllism@gmail.com>
2018-12-02 17:31:25 -0600

    add images to the carosel

--- client/src/App.js
+++ client/src/App.js
@@ -3,14 +3,8 @@ import { BrowserRouter, Route } from 'react-router-dom';
 
 
 // COMPONENTS
-
 import Landing from './components/Landing';
-import Hero from "./components/hero/hero";
 import Navbar from "./components/navbar/navbar";
-import SideDrawer from "./components/navbar/sideDrawer";
-import Backdrop from "./components/navbar/backdrop";
-import Mission from "./components/mission/mission";
-import Info from './components/info/info';
 import TeamCard from './components/team/team';
 import Footer from './components/footer/footer';
 
