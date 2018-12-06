44e3ef46d68922be35de936e15f2a47b7c0e2862
Johnathan Chen <johnathanchenn@outlook.com>
2018-12-05 22:50:16 -0800

    edit giant hand

--- client/src/components/hero/hero.js
+++ client/src/components/hero/hero.js
@@ -4,8 +4,7 @@ import './hero.css'
 class Hero extends Component {
   render() {
     return(
-      <div>
-        <span className="hero">
+      <div className="hero">
           {/*<img src={logo} className="App-logo" alt="logo" />*/}
           <h1>Welcome to Paysplit</h1>
           <p>An easy way to split the bill between friends</p>
@@ -13,7 +12,6 @@ class Hero extends Component {
             { /*change this redirect link when you have the launched address */}
           </div>
 	        <button className={'hero-btn'}><a href='/'>Try Paysplit</a></button>
-        </span>
       </div>
     )
   }
